06dffefebb0827fd9b2f05fd2ab7bd016bfde751
Leon <Leon.geim@googlemail.com>
2017-12-02 11:15:06 +0100

    asdasd

--- Dreicksausgabe/main.c
+++ Dreicksausgabe/main.c
@@ -9,17 +9,20 @@ void main()
 
 	for (i = 1; i <= anzahlZeilen; i++)
 	{
+		//Erstes Dreieck
 		for (j = anzahlZeilen - i; j < anzahlZeilen; j++)
 		{
 			printf("*");
 
 		}
 
+		//Leerzeichen in Relation zur gesamten Anzahl der Zeilen und der aktuellen Zeile
 		for (temp = anzahlZeilen * 2; temp >= i * 2; temp--)
 		{
 			printf(" ");
 		}
 
+		//Zweites Dreieck
 		for (j = anzahlZeilen - i; j < anzahlZeilen; j++)
 		{
 			printf("*");
@@ -33,20 +36,23 @@ void main()
 
 	for (i = 1; i <= anzahlZeilen; i++)
 	{
+		//3tes Dreieck
 		for (j = 0 + i; j <= anzahlZeilen; j++)
 		{
 			printf("*");
 		}
 
-
-		for (temp = 0; temp < 0 + i; temp++)
+		//So viele Leerzeichen wie die Aktuelle Zeile - 1
+		for (temp = 0; temp < i; temp++)
 		{
 			printf(" ");
 		
+			//Nach dem ersten Leerzeichen müssen immer 2 gemacht werden
 			if (temp > 0)
 				printf(" ");
 		}
 
+		//4tes Dreieck
 		for (j = 0 + i; j <= anzahlZeilen; j++)
 		{
 			printf("*");
